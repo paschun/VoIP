d9f33b31b6aaf27c90db3cc52343eb97dfdcdaeb
paschun <152430265+paschun@users.noreply.github.com>
2026-05-20 01:49:54 -0700

    replace rimraf with node.fs

--- frontend/build/build.js
+++ frontend/build/build.js
@@ -4,7 +4,7 @@ require('./check-versions')()
 process.env.NODE_ENV = 'production'
 
 const ora = require('ora')
-const rm = require('rimraf')
+const fs = require('node:fs/promises')
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
@@ -14,8 +14,7 @@ const webpackConfig = require('./webpack.prod.conf')
 const spinner = ora('building for production...')
 spinner.start()
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
+fs.rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), { recursive: true, force: true }).then(() => {
   webpack(webpackConfig, (err, stats) => {
     spinner.stop()
     if (err) throw err
@@ -38,4 +37,4 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       '  Opening index.html over file:// won\'t work.\n'
     ))
   })
-})
+}).catch(err => { throw err })
