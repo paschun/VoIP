b3bb7343f0a583239cce4317a0c88b0e3f6e4d45
paschun <152430265+paschun@users.noreply.github.com>
2026-05-23 01:37:31 -0700

    Fix rateLimit constructor

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const path = require('node:path')
+const http = require('node:http')
+const { Server } = require('socket.io')
 const express = require('express')
-const app = express()
-const cors = require("cors");
-const path = require('path');
+const { rateLimit } = require('express-rate-limit')
+const helmet = require('helmet')
+const cors = require('cors')
 const session = require('cookie-session')
 const compression = require('compression')
+const mongoose = require('./config/db.config');
+
+const app = express()
 app.use(compression())
 
-var expiryDate = new Date(Date.now() + 60 * 60 * (1000 * 12 * 30)) // 30 day
+const expiryDate = new Date(Date.now() + 60 * 60 * (1000 * 12 * 30)) // 30 day
 app.use(session({
   name: 'session',
   keys: [process.env.COOKIE_KEY], // could hypothetically have process.env.COOKIE_KEY2 , but need to change other refs
@@ -17,9 +23,9 @@ app.use(session({
   }
 }))
 
-let setCache = function (req, res, next) {
+const setCache = (req, res, next) => {
   const period = 60 * 60 * 24
-  if (req.method == 'GET') {
+  if (req.method === 'GET') {
     res.set('Cache-control', `public, max-age=${period}`)
   } else {
     res.set('Cache-control', `no-store`)
@@ -28,8 +34,6 @@ let setCache = function (req, res, next) {
 }
 app.use(setCache)
 
-const RateLimit = require('express-rate-limit');
-const helmet = require("helmet");
 app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
@@ -54,31 +58,28 @@ app.use(helmet.referrerPolicy());
 app.use(helmet.xssFilter());
 app.disable('x-powered-by');
 app.set('trust proxy', 1)
-const server = require('http').createServer(app);
-
-global.io = require('socket.io')(server,{ cors: { origin: '*' } });
-
-const mongoose = require('./config/db.config');
+const server = http.createServer(app);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
   console.log('database connected successfully!');
 });
 
 //app.use(cors());
 app.use(cors({ origin: ['http://localhost:8080'], }))
 
-const limiter = new RateLimit({
+const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   limit: 100,
   message: "Slow down your requests!",
   legacyHeaders: false,
   standardHeaders: 'draft-8',
 });
-  
 // apply rate limiter to all requests
 app.use(limiter);
+
+global.io = Server(server, { cors: { origin: '*' } });
 io.on('connection', socket => {
   console.log('a user connected');
   socket.on('join_channel', (channel) => {
@@ -94,10 +95,10 @@ io.on('connection', socket => {
 app.use('/frontend/dist/index.html', express.static('frontend/dist/index.html'));
 app.use('/version.md', express.static('version.md'));
 // app.enable('trust proxy')
-if( process.env.HTTPS.trim() === 'true'){
+if (process.env.HTTPS.trim() === 'true') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https'){
-      if(req.url == '/get-base-url'){
+      if(req.url === '/get-base-url'){
         next()
         // res.status(200).json({url: process.env.BASE_URL.trim()});
       }else{
@@ -130,13 +131,13 @@ app.use('/src', express.static('src'));
 app.use('/frontend', express.static('frontend'));
 // app.use('/frontend', express.static('frontend'));
 app.use('/frontend/dist/static/', express.static('frontend/dist/static'));
-app.get(`/error`, function (req, res) {
+app.get(`/error`, (_req, res) => {
   res.sendFile(path.join(__dirname, './error/index.html'));
 })
-app.get(`/:id`, function (req, res) {
+app.get(`/:id`, (_req, res) => {
   res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
 })
-app.get(`/:id/:name`, function (req, res) {
+app.get(`/:id/:name`, (_req, res) => {
   res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
 })
 app.use(express.static(path.join(__dirname, './frontend/dist')));
@@ -151,11 +152,11 @@ require("./app/routes/call.route")(app);
 require("./app/routes/hardwarekey.route")(app);
 
 
-app.get('/api/users/', function (req, res) {
+app.get('/api/users/', (_req, res) => {
   res.status(200).json({message: 'success'});
 })
 
-app.get('/get-base-url', function(req, res) {
+app.get('/get-base-url', (_req, res) => {
   res.status(200).json({url: process.env.BASE_URL.trim()});
 });
 console.log('express listening on PORT', process.env.PORT)
