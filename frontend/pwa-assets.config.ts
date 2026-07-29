4a5f0c03c6df8bfa67d463fd7cfdfc25bfc32a13
paschun <152430265+paschun@users.noreply.github.com>
2026-07-28 23:21:19 -0700

    Use dark background for homescreen icons

--- frontend/pwa-assets.config.ts
+++ frontend/pwa-assets.config.ts
@@ -5,8 +5,9 @@ export default defineConfig({
   // https://vite-pwa-org.netlify.app/assets-generator/cli.html#configurations
   preset: {
     transparent: { sizes: [192, 512], favicons: [[32, 'favicon.ico']] },
-    maskable: { sizes: [512], padding: 0.4 }, // default padding is 0.3
-    apple: { sizes: [180] },
+    // default background is white for homescreen icons, replace with theme_color
+    maskable: { sizes: [512], padding: 0.4, resizeOptions: { background: '#2e2e2e' } }, // default padding is 0.3
+    apple: { sizes: [180], resizeOptions: { background: '#2e2e2e' } },
   } satisfies Preset,
   images: ['public/chat-cat.svg'],
 })
