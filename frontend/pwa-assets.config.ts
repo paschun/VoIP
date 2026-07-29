167972944cc84f4f41a20f2904d8cc3cf30debeb
paschun <152430265+paschun@users.noreply.github.com>
2026-07-28 23:09:25 -0700

    Our maskable icon needs a little extra padding

--- frontend/pwa-assets.config.ts
+++ frontend/pwa-assets.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from '@vite-pwa/assets-generator/config'
+import { defineConfig, type Preset } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
   headLinkOptions: { preset: '2023' },
   // https://vite-pwa-org.netlify.app/assets-generator/cli.html#configurations
   preset: {
     transparent: { sizes: [192, 512], favicons: [[32, 'favicon.ico']] },
-    maskable: { sizes: [512] },
+    maskable: { sizes: [512], padding: 0.4 }, // default padding is 0.3
     apple: { sizes: [180] },
-  },
+  } satisfies Preset,
   images: ['public/chat-cat.svg'],
 })
